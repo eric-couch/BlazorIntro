605aca4767893f1aed666e173b831a00e508c352
Eric Couch <ericcouch@yahoo.com>
2023-03-16 14:07:48 -0500

    Add project files.

---
+++ BlazorIntro/Server/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlazorIntro.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+
+
+    }
+}
