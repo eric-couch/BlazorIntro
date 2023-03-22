026717d3c3f786161262d1fffe4df583cef255cd
Eric Couch <ericcouch@yahoo.com>
2023-03-22 14:57:32 -0500

    updated removemovie

--- BlazorIntro/Server/Controllers/UserController.cs
+++ BlazorIntro/Server/Controllers/UserController.cs
@@ -78,21 +78,7 @@ namespace BlazorIntro.Server.Controllers
             _context.SaveChangesAsync();
             return Ok();
         }
-
-        //[HttpGet]
-        //[Route("api/get-admin-status")]
-        //public async Task<ActionResult> GetAdminStatus() {
-        //    var currentUser = await _userManager.GetUserAsync(User);
-
-        //    var isAdmin = await _userManager.IsInRoleAsync(currentUser, "Admin");
-        //    if (isAdmin) {
-        //        return Ok("You are an admin");
-        //    } else
-        //    {
-        //        return Unauthorized("You are not authroized to access this resource.");
-        //    }
-        //}
-
+        
         [HttpGet]
         [Route("api/get-roles")]
         public async Task<IActionResult> GetUserRoles()
@@ -102,20 +88,26 @@ namespace BlazorIntro.Server.Controllers
                 // Get the current user
                 var user = await _userManager.FindByIdAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
 
-                // Get the roles for the current user
-                var roles = await _userManager.GetRolesAsync(user);
-
-                return Ok(roles);
+                if (user is not null)
+                {
+                    // Get the roles for the current user
+                    var roles = await _userManager.GetRolesAsync(user);
+                    return Ok(roles);
+                }
+                else
+                {
+                    return NotFound();
+                }
             }
             catch (Exception ex)
             {
                 return Problem(
-                //all parameters are optional:
-                detail: "Error while retrieving roles.", //an explanation, ex.Stacktrace, ...
-                //instance: $"/user/{user.id}"  //A reference that identifies the specific occurrence of the problem
-                title: "An error occured.", //a short title, maybe ex.Message
-                statusCode: StatusCodes.Status500InternalServerError //will always return code 500 if not explicitly set
-                //type: "http://example.com/errors/error-123-details"  //a reference to more information
+                    //all parameters are optional:
+                    detail: "Error while retrieving roles.", //an explanation, ex.Stacktrace, ...
+                    //instance: $"/user/{user.id}"  //A reference that identifies the specific occurrence of the problem
+                    title: "An error occured.", //a short title, maybe ex.Message
+                    statusCode: StatusCodes.Status500InternalServerError //will always return code 500 if not explicitly set
+                    //type: "http://example.com/errors/error-123-details"  //a reference to more information
                 );
             }
         }
